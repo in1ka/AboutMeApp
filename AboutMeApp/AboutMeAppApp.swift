f953a557cb8dc5a3926b29d35d8dccd410f32184
Inika Bhargava <inikamehro@gmail.com>
2025-04-19 12:26:59 -0700

    Initial Commit

---
+++ AboutMeApp/AboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeAppApp.swift
+//  AboutMeApp
+//
+//  Created by Inika Bhargava on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
